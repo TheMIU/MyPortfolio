64438038bee013da014c549913f7820f90c4d2e1
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-06-03 18:06:59 +0530

    start stop buttons added for que

--- Assignments/JS/QUE/assets/js/JavaScript.js
+++ Assignments/JS/QUE/assets/js/JavaScript.js
@@ -1,15 +1,29 @@
-
 let colors = ["red", "yellow", "green", "blue", "deeppink", "orange"];
 let divArray = document.querySelector("section").querySelectorAll("div");
+let isStared = false;
 
-setInterval(function (){
-   for (let i = 0; i<divArray.length; i++){
-      console.log(divArray[i]);
-      divArray[i].style.background = colors[i];
-   }
-   let a = colors.pop();
-   colors.unshift(a);
-},1000);
+setInterval(function () {
+    if(isStared){
+        for (let i = 0; i < divArray.length; i++) {
+           // console.log(divArray[i]);
+            divArray[i].style.background = colors[i];
+        }
+        let a = colors.pop();
+        colors.unshift(a);
+    }else {
+        for (let i = 0; i < divArray.length; i++) {
+            divArray[i].style.background = colors[i];
+        }
+    }
 
-let arr = ["A","B","C","D"];
+}, 300);
 
+let arr = ["A", "B", "C", "D"];
+
+function startQue() {
+    isStared = true;
+}
+
+function stopQue() {
+    isStared = false;
+}
