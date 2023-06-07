510b104173409a1b06c142355fde9983a5be59b6
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-06-07 13:45:01 +0530

    Updated with array

--- Assignments/JS/InMemoryPOS/assets/js/JavaScript.js
+++ Assignments/JS/InMemoryPOS/assets/js/JavaScript.js
@@ -1,3 +1,5 @@
+// Navigation
+
 let login = document.getElementById('login');
 let navBar = document.getElementById('navigation-bar');
 let home = document.getElementById('home');
@@ -81,74 +83,19 @@ function goHome() {
     place_order.style.display = 'none';
 }
 
-/*/////////////////////////////////////////*/
-//select the button and add a event to it
-/*
-document.getElementById("btnCustomer").addEventListener("click", function () {
-*/
 
+//  set input field values to table
 
-/*$("#btnCustomer").click( function () {
-    //get typed values of input fields
-    // let id = document.getElementById("txtCustomerID").value;
+let customerArray = [];
+
+$("#btnCustomer").click(function () {
     let id = $("#txtCustomerID").val();
     let name = $("#txtCustomerName").val();
     let address = $("#txtCustomerAddress").val();
     let salary = $("#txtCustomerSalary").val();
 
-    //catch the table body
     let tBody = $("#tblCustomer");
-    //  document.getElementById("tblCustomer");
-
-    // let's see how we should insert a table row using html format
-    //<tr>
-    // <td>id</td>
-    // <td>name</td>
-    // <td>address</td>
-    // <td>salary</td>
-    // </tr>
-
-    //ok then, let's create the above row
-    let tr = document.createElement("tr");
-
-    //let's create another four columns
-    let col1 = document.createElement("td");
-    let col2 = document.createElement("td");
-    let col3 = document.createElement("td");
-    let col4 = document.createElement("td");
-
-    //set input fields values to the above created columns
-    col1.textContent = id;
-    col2.textContent = name;
-    col3.textContent = address;
-    col4.textContent = salary;
-
-    //set 4 columns to the row
-    //set 4 columns to the previously created row
-    tr.appendChild(col1);
-    tr.appendChild(col2);
-    tr.appendChild(col3);
-    tr.appendChild(col4);
-
-    //set the tr to the table body
-    //set the row to the table body
+    let tr = $("<tr> <td>" + id + "</td> <td>" + name + "</td> <td>" + address + "</td><td>" + salary + "</td> </tr>");
+    customerArray.push({id, name, address, salary});
     tBody.append(tr);
-
-    /!*
-
-        // Get the tbody element
-        var tbody = $("#tbody");
-
-    // Create a new tr element
-        var tr = $("<tr>");
-
-    // Add some child elements to the tr element
-        tr.append("<td>1</td>");
-        tr.append("<td>2</td>");
-
-    *!/
-});*/
-
-
-//////////////////////////////////////////////
-
+});
