c9e4a63d5da32f54cdbe88bee43f262c6fcec2f3
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-06-25 13:48:11 +0530

    load items to place order table

--- Assignments/JS/InMemoryPOS/controller/OrderController.js
+++ Assignments/JS/InMemoryPOS/controller/OrderController.js
@@ -84,15 +84,126 @@ selectCodeElement.addEventListener("change", function () {
 var orderQtyInput = $("#OrderItemQty");
 var totalOfItem = $("#TotalOfItem");
 
+
 orderQtyInput.on("keyup", function () {
     var price = parseFloat($('#OrderItemPrice').val());
-    var orderQty = parseFloat(orderQtyInput.val());
+    var orderQty = parseInt(orderQtyInput.val());
 
-    if (!isNaN(price) && !isNaN(orderQty)) {
+    if (!isNaN(price) && !isNaN(orderQty) && isValidOrderItemQty) {
         var total = price * orderQty;
         totalOfItem.val(total.toFixed(2));
+
     } else {
         totalOfItem.val("");
     }
+
 });
 
+
+////////////////////////////////////////////////
+let cart = [];
+
+$('#addToCart').click(function () {
+    // Get the item details
+    let itemCode = $('#selectCode').val();
+    let itemName = $('#OrderItemName').val();
+    let price = $('#OrderItemPrice').val();
+    let quantity = parseInt($('#OrderItemQty').val()); // Convert to number
+    let total = parseFloat(price) * quantity; // Convert to number
+
+    // Check if the item already exists in the cart
+    let existingItem = cart.find(function (item) {
+        return item.itemCode === itemCode;
+    });
+
+    if (existingItem && isValidOrderItemQty) {
+        // Update the quantity and total of the existing item
+        existingItem.quantity += quantity;
+        existingItem.total += total;
+    } else {
+        // Create a new cart item object
+        let cartItem = {
+            itemCode: itemCode,
+            itemName: itemName,
+            price: price,
+            quantity: quantity,
+            total: total
+        };
+
+        // Add the cart item to the cart array
+        if (isValidOrderItemQty) {
+            cart.push(cartItem);
+        } else {
+            $('#OrderItemQty').focus();
+            return; // Exit the function without displaying the cart items
+        }
+    }
+
+    // Display the cart items in the table
+    displayCartItems();
+
+    // clear qty and total
+    $('#OrderItemQty').val("");
+    $('#TotalOfItem').val("");
+
+    isValidOrderItemQty = false;
+
+});
+
+//add to table
+function displayCartItems() {
+    let cartTable = $('#cart');
+
+    // Clear the table body
+    cartTable.find('tbody').empty();
+
+    // Iterate over the cart items and add rows to the table
+    cart.forEach(function (item) {
+        let row = '<tr>' +
+            '<td>' + item.itemCode + '</td>' +
+            '<td>' + item.itemName + '</td>' +
+            '<td>' + item.price + '</td>' +
+            '<td>' + item.quantity + '</td>' +
+            '<td>' + item.total + '</td>' +
+            '</tr>';
+
+        cartTable.find('tbody').append(row);
+    });
+}
+
+// load selected row to fields
+$('#cart tbody').on('click', 'tr', function () {
+    // Get the data from the clicked row
+    let itemCode = $(this).find('td:eq(0)').text();
+    let itemName = $(this).find('td:eq(1)').text();
+    let price = $(this).find('td:eq(2)').text();
+    let quantity = $(this).find('td:eq(3)').text();
+
+    // Populate the input fields with the data
+    $('#selectCode').val(itemCode);
+    $('#OrderItemName').val(itemName);
+    $('#OrderItemPrice').val(price);
+    $('#OrderItemQty').val(quantity);
+});
+
+// Row selection
+$('#cart tbody').on('click', 'tr', function() {
+    $(this).toggleClass('selected');
+});
+
+// Remove selected row
+$('#RemoveSelected').click(function () {
+    // Get the selected row
+    let selectedRow = $('#cart tbody tr.selected');
+
+    if (selectedRow.length > 0) {
+        // Get the index of the selected row within the table
+        let selectedRowIndex = selectedRow.index();
+
+        // Remove the selected row from the table
+        selectedRow.remove();
+
+        // Remove the corresponding item from the cart array
+        cart.splice(selectedRowIndex, 1);
+    }
+});
--- Assignments/JS/InMemoryPOS/controller/validations/OrderValidations.js
+++ Assignments/JS/InMemoryPOS/controller/validations/OrderValidations.js
@@ -1,20 +1,15 @@
-let isValidOrderItemQty;
+let isValidOrderItemQty = false;
 let orderItemQtyField = $('#OrderItemQty');
 let orderItemQtyOnH = $('#OrderItemQtyOnH');
 
 // regex pattern
-let regexItemQty = /^\d+(\.\d{1,2})?$/;
+let regexItemQty = /^(?!0+(\.0+)?$)\d+(\.\d{1,2})?$/;
 
 orderItemQtyField.on('keyup', function () {
     let enteredQty = parseInt(orderItemQtyField.val());
     let availableQty = parseInt(orderItemQtyOnH.val());
 
     isValidOrderItemQty = isValid(regexItemQty, orderItemQtyField.val()) && (enteredQty <= availableQty);
-
-    console.log(enteredQty <= availableQty);
-    console.log(enteredQty);
-    console.log(availableQty);
-    console.log(isValidOrderItemQty);
 
     changeTextFieldColors(orderItemQtyField,isValidOrderItemQty);
 });
